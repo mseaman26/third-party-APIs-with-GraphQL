77c46aca2c3da12d606d3133a47421b7645a387f
michael seaman <mseaman26@gmail.com>
2024-06-27 18:41:23 -0700

    env variables

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,13 @@
 const { User } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
+const process = require('process')
 
 var geoTrackingAPIKey = process.env.WEATHER_API_KEY
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      
+      console.log('api key: ', geoTrackingAPIKey)
       try{
         const geoResponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=san%20francisco&appid=${geoTrackingAPIKey}`, {
           method: 'GET',
@@ -25,26 +26,25 @@ const resolvers = {
       const geoData = await geoResponse.json()
       const lat = geoData.coord.lat
       const lon = geoData.coord.lon
+       var CurrentWeatherMapURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${geoTrackingAPIKey}`
+      const weatherResponse = await fetch(CurrentWeatherMapURL, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      if (!weatherResponse.ok) {
+        console.log('Failed to get weather data')
+      }
+      const weatherData = await weatherResponse.json()
+      console.log('weatherData: ', weatherData)
       console.log('geoData: ', geoData)
       console.log(lat, lon)
         
       }catch(e){
         console.log('geoerror: ', e)
       }
- 
-      // const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${geoTrackingAPIKey}`, {
-      //   method: 'GET',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      // })
-      // if (!weatherResponse.ok) {
-      //   console.log('Failed to get weather data')
-      // }
-      // const weatherData = await weatherResponse.json()
-      // console.log(weatherData)
-      
-      // console.log(lat, lon)
+
       if (context.user) {
         return await User.findOne({ _id: context.user._id });
       }
